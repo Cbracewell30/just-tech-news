8970c643499a7bdd3f6425f409225c405d20524a
Chris <chrisbracewell35@gmail.com>
2022-02-22 19:15:23 -0500

    adding place holder

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
 
 // create our User model
 class User extends Model {}
